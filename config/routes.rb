3a78da63d4435898cd226e3acb825d8ebc343097
Hikaru1209 <h.nito2310@gmail.com>
2020-05-28 20:28:17 +0900

    [Edit]Task2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: "top#top"
+delete 'books/:id' => 'books#destroy', as: 'destroy_books'
 resources :books
+post "books" => "books#create"
+get "books/:id" => "books#show"
 end
